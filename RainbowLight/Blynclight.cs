10593a02d57d77f06f4c6695909a6a2a8460cc12
Sam Lord <sam.lord@squaredup.com>
2016-11-03 10:28:36 +0000

    Initial commit

---
+++ RainbowLight/Blynclight.cs
@@ -0,0 +1,77 @@
+﻿using Blynclight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RainbowLight
+{
+    class Blynclight
+    {
+        private BlynclightController controller;
+
+        private int deviceCount;
+
+        private double val;
+        private double sat;
+        private double hue;
+
+        private object valLock = new object();
+        public Blynclight()
+        {
+            hue = 0.0;
+            val = 1.0;
+            sat = 1.0;
+
+            controller = new BlynclightController();
+            deviceCount = controller.InitBlyncDevices();
+
+            Thread thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    Thread.Sleep(10);
+                    decreaseBrightness();
+                }
+            });
+            thread.Start();
+        }
+
+        public void changeColor()
+        {
+            Random rand = new Random();
+            lock (valLock)
+            {
+                hue = rand.NextDouble() * 360;
+                val = 0.6; // 0.6 is a nice brightness
+                setColor();
+            }
+        }
+        private void setColor()
+        {
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+
+            Color.HsvToRgb(hue, sat, val, out red, out green, out blue);
+
+            controller.TurnOnRGBLights(deviceCount - 1, (byte)red, (byte)green, (byte)blue);
+        }
+
+        private void decreaseBrightness()
+        {
+            lock (valLock)
+            {
+                val -= 0.01;
+                if (val < 0)
+                {
+                    val = 0;
+                }
+                
+                setColor();
+            }
+        }
+    }
+}
---
+++ RainbowLight/KeyboardHook.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RainbowLight
+{
+    class KeyboardHook
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private LowLevelKeyboardProc _proc;
+        private IntPtr _hookID = IntPtr.Zero;
+
+        private Blynclight light;
+        public KeyboardHook(Blynclight light)
+        {
+            this.light = light;
+            _proc = HookCallback;
+
+            _hookID = SetHook(_proc);
+            Application.Run();
+            UnhookWindowsHookEx(_hookID);
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                light.changeColor();
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(13, proc, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+        LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+}
